40a0a182d3dee2753c0ad8cd664c563946b9297b
kris-tran <tungtran2222@gmail.com>
2021-04-05 17:47:55 +0700

    Change test

--- src/test/java/io/tungttran/coronavirustracker/CoronavirusTrackerApplicationTests.java
+++ src/test/java/io/tungttran/coronavirustracker/CoronavirusTrackerApplicationTests.java
@@ -1,4 +1,4 @@
-package io.javabrains.coronavirustracker;
+package io.tungttran.coronavirustracker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
